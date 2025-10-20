6baa6ec64b7d267d26105596eff540b7ff7ed3f3
Nicole Costa Dev <nicolecoosta.dev@gmail.com>
2025-10-19 23:35:13 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base diferente pra dev e prod (GH Pages precisa do subpath)
-  base: process.env.NODE_ENV === "production" ? "/siingulo/" : "/",
+  base: "/siingulo/",
   build: { outDir: "docs" },
 });
